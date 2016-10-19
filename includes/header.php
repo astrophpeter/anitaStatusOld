63a7ffaf0c759425387b744a9bcbfa4c751c849e
Peter McGill <petermcgill94@gmail.com>
2016-10-19 18:00:04 +0100

    nav bar done

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,6 @@ echo'
 <nav class="navbar navbar-light">
   <a class="navbar-brand" href="index.php">
     <img src="assets/logo_trans.png" style="display: inline-block;">
-    <span style="display: inline-block;">My Company</span>
 </a>
 </nav>
 ';
