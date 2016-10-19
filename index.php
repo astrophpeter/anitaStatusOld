56c20d36c56cf8930d0635442952070ba38a38f9
Peter McGill <petermcgill94@gmail.com>
2016-10-19 18:44:08 +0100

    nav

--- index.php
+++ index.php
@@ -26,7 +26,14 @@
       </div>
         <div class="row">
           <div class="col-lg-10">
-            <div class="alert alert-info" role="alert">Flight Status Live ...</div>
+            <ul class="nav nav-tabs">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">Data Flow</a></li>
+              <li><a href="#">Power</a></li>
+              <li><a href="#">Memory</a></li>
+              <li><a href="#">Altitude</a></li>
+            </ul>
           </div>
           <div class="col-lg-2">
             <div class="alert alert-info" role="alert">Flight Status Live ...</div>
