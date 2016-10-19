560599e9d3045b3c147682e3462c29716920ed0c
Peter McGill <petermcgill94@gmail.com>
2016-10-19 17:57:58 +0100

    bg

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
 echo'
-<nav class="navbar navbar-light bg-faded">
-  <a class="navbar-brand" href="#">
+<nav class="navbar navbar-light">
+  <a class="navbar-brand" href="index.php">
     <img src="assets/logo_trans.png" style="display: inline-block;">
     <span style="display: inline-block;">My Company</span>
 </a>
