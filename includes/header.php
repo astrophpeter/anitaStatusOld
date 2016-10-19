afe4a0fc4d27aefe1b2a7b522748685d4aa4f367
Peter McGill <petermcgill94@gmail.com>
2016-10-19 17:55:44 +0100

    brand

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,10 @@
 <?php
 echo'
 <nav class="navbar navbar-light bg-faded">
-<a class="navbar-brand" href="http://disputebills.com"><img src="assets/logo_trans.png" alt="Dispute Bills">
-      </a>
+  <a class="navbar-brand" href="#">
+    <img src="assets/logo_trans.png" style="display: inline-block;">
+    <span style="display: inline-block;">My Company</span>
+</a>
 </nav>
 ';
 ?>
