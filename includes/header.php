75c195a31bfc76eaf2e3657a20d553bb61d16a81
Peter McGill <petermcgill94@gmail.com>
2016-10-19 18:20:32 +0100

    nav

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
 echo'
-<nav class="navbar navbar-light">
+<nav class="navbar navbar-default">
   <a class="navbar-brand" href="index.php">
     <img src="assets/logo_trans.png" style="display: inline-block;">
 </a>
