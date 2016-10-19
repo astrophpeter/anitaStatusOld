218ac9318293dc8d72714a8bb7b3bc0ba5bc8a91
Peter McGill <petermcgill94@gmail.com>
2016-10-19 17:47:04 +0100

    adjust logo

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 echo'
 <nav class="navbar navbar-light bg-faded">
   <a class="navbar-brand" href="#">
-    <img src="/assets/logo_trans.png" width="30" height="30" alt="">
+    <img src="/assets/logo_trans.png" width="120" height="50" alt="">
   </a>
 </nav>
 ';
