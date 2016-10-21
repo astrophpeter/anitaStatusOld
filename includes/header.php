0f7da2dcccc949f51024d520c180b3439010a90e
Peter McGill <petermcgill94@gmail.com>
2016-10-21 15:53:13 +0100

    added my css fixed nav bar height

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 echo'
 <nav class="navbar navbar-default">
   <a class="navbar-brand" href="index.php">
-    <img src="assets/logo_trans.png" style="display: inline-block;height:100px;">
+    <img src="assets/logo_trans.png" style="display: inline-block;height:50px;">
 </a>
 </nav>
 ';
