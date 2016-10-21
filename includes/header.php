482edfbd503fc7135dfd74dd542459708196d741
Peter McGill <petermcgill94@gmail.com>
2016-10-21 16:12:24 +0100

    added my css fixed nav bar height

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,11 @@
 <?php
 echo'
 <nav class="navbar navbar-default">
-  <nav class="navbar-header">
+  <div class="navbar-header">
     <a class="navbar-brand" href="index.php">
       <img src="assets/logo_trans.png">
     </a>
-  </nav>
+  </div>
 </nav>
 ';
 ?>
