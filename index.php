ef54f87f0d13ce3734e9930e89fa5ffdb59269d9
Peter McGill <petermcgill94@gmail.com>
2016-10-24 17:59:46 +0100

    banner.php content added

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/myccs.css" rel="stylesheet">
