329c476c4513984bf498e996eede368856d7d412
Peter McGill <petermcgill94@gmail.com>
2016-10-24 14:27:24 +0100

    banner.php content added

--- includes/banner.php
+++ includes/banner.php
@@ -5,15 +5,15 @@ echo '
   <div class="row">
 
     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        Flight Status: <img src="assets/live.gif"/> Live
+        <b>Flight Status:</b> <img src="assets/live.gif"/> Live
     </div>
 
     <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-        Flight Duration: 1 Day 34 hours 32 s
+        <b>Flight Duration:</b> 1 Day 34 hours 32 s
     </div>
 
     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-       Last Packet Recieved: 12/10/16:13:34
+       <b>Last Packet Recieved:</b> 12/10/16:13:34
     </div>
 
 </div>
