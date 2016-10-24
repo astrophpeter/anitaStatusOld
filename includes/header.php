c5e009673f1220b0798031bdf48e19bd461618e9
Peter McGill <petermcgill94@gmail.com>
2016-10-24 11:54:04 +0100

    removed links from nav

--- includes/header.php
+++ includes/header.php
@@ -2,22 +2,11 @@
 echo'
 <nav class="navbar navbar-default">
     <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-
-        <a class="navbar-brand" href="#"><img src="assets/logo_trans.png" height="100" /></a>
+        <a class="navbar-brand" href="index.php"><img id="logo" src="assets/logo_trans.png"/></a>
     </div>
 
-    <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li><a href="">Portfolio</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
+    <div class="navbar hidden-tablet hidden-phone">
+        <h4> Web Moniter </h4>
     </div>
 </nav>
 ';
