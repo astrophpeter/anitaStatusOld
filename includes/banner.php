7a22bf65946b366bbccc22cfc071601eca25f4f0
Peter McGill <petermcgill94@gmail.com>
2016-10-24 14:05:31 +0100

    banner.php content added

--- includes/banner.php
+++ includes/banner.php
@@ -1,7 +1,23 @@
 <?php
-
 echo '
-  <div class="alert alert-info" role="alert">Flight Status: <img src="assets/live.gif"/> Live</div>
+
+<div class="alert alert-info" role="alert">
+  <table class="table">
+    <tbody>
+      <tr>
+
+        <th scope="row"></th>
+
+        <td>Flight Status: <img src="assets/live.gif"/> Live<</td>
+
+        <td>Flight Duration: 1 Day 34 hours 32 s</td>
+
+        <td>Last Packet Recieved: 12/10/16:13:34</td>
+
+      </tr>
+    </tbody>
+  </table>
+
 '
 
 ?>
