9b8017cab7f3271178ae73f857fb5304caf1380d
Peter McGill <petermcgill94@gmail.com>
2016-10-24 14:25:07 +0100

    banner.php content added

--- includes/banner.php
+++ includes/banner.php
@@ -4,11 +4,11 @@ echo '
 <div class="alert alert-info" role="alert">
   <div class="row">
 
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
         Flight Status: <img src="assets/live.gif"/> Live
     </div>
 
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
         Flight Duration: 1 Day 34 hours 32 s
     </div>
 
