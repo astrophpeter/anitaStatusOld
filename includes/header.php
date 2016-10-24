afd472b4ccf9c533a0837b9adf14ce79da50bfa6
Peter McGill <petermcgill94@gmail.com>
2016-10-24 11:59:37 +0100

    spacing on navbar

--- includes/header.php
+++ includes/header.php
@@ -7,8 +7,6 @@ echo'
     </div>
     </div>
     <a class="navbar-brand" href="index.php"><img id="logo" src="assets/logo_trans.png"/></a>
-        <div class="navbar hidden-tablet hidden-phone">
-            <h4> Status Page </h4>
     </div>
 </nav>
 ';
